9f6afe321d24b895da8dc93527b410dbfb8e6452
Julia Georgiew <java2011@me.com>
2023-10-05 21:17:38 +0200

    model and serializer

---
+++ todo/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import TodoSerializer
+from .models import Todo
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Todo.objects.all().order_by('-created_at')
+    serializer_class = TodoSerializer
+    permission_classes = []  #permissions.IsAuthenticated
